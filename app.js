bf1cccbbad797c6eb5c23b18d7c8f1e22cdb99ac
MikeYencha <jmyeniii@icloud.com>
2017-09-20 19:50:10 -0400

    javascript update

---
+++ app.js
@@ -0,0 +1,11 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAwQb-VKwtm4vEPQGY9nxjxoU0G_XonCWg",
+    authDomain: "time-f5df0.firebaseapp.com",
+    databaseURL: "https://time-f5df0.firebaseio.com",
+    projectId: "time-f5df0",
+    storageBucket: "time-f5df0.appspot.com",
+    messagingSenderId: "161840084912"
+  };
+  firebase.initializeApp(config);
